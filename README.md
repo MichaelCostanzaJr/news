a70adc97d2a283fe78a80a21cb779b368b9a2ed2
Costanzamike96 <costanzamike96@gmail.com>
2022-07-30 16:12:21 -0700

    Debug secret key

---
+++ README.md
@@ -0,0 +1,1 @@
+https://safe-retreat-18824.herokuapp.com/
--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = OmFRqtfA-ePbP89xZts9vEk7rscL3A8z3FL-wQhSH4w
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
