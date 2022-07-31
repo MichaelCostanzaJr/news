1b711196a6927c59bb10407ed8df2d6e710c4537
Costanzamike96 <costanzamike96@gmail.com>
2022-07-31 15:01:54 -0700

    Updated pages/home

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path('',views.ArticleListView.as_view(), {"section": 1, "status": 1}, name="home"),
+    path('',views.ArticleListView.as_view(), {"section": 1, "status": 1}, name="article_home"),
     path('<int:section>/<int:status>/', views.ArticleListView.as_view(), name="article_list"),
     path('<int:pk>/', views.ArticleDetailView.as_view(), name="article_detail"),
     path('<int:pk>/edit/', views.ArticleUpdateView.as_view(), name="article_edit"),
